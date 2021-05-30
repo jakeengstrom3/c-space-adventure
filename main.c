46b29515a01627f3c9dd99219b50efe9a72e042d
jakeengstrom3 <jakeengstrom3@DESKTOP-27NSSQD.localdomain>
2021-05-30 14:41:12 -0700

    Getting Basic I/O working. Cookbook Method coming along nicely

--- main.c
+++ main.c
@@ -1,6 +1,25 @@
 #include <stdio.h>
+const int MAX_INPUT_SIZE = 256;
+void print_welcome_message();
+const char* get_name();
+
 int main(){
     
-    printf("hello, world");
+    
+    print_welcome_message();
+    get_name();
+
     return 0;
+}
+
+void print_welcome_message(){
+    printf("Hello and welcome to SPACE ADVENTURES!!!!!\n");
+}
+
+const char* get_name(){
+    char name[MAX_INPUT_SIZE];
+    printf("Please Enter Your Name: ");
+    fgets(name, MAX_INPUT_SIZE, stdin);
+    printf("\nWelcome %s\n", name);
+    
 }
