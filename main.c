93dd037ff6b70579f5280e014d564a510e1e5378
jakeengstrom3 <jakeengstrom3@DESKTOP-27NSSQD.localdomain>
2021-05-30 23:59:32 -0700

    Added more cookbook functions. Added planet.json file.

--- main.c
+++ main.c
@@ -1,14 +1,13 @@
 #include <stdio.h>
 const int MAX_INPUT_SIZE = 256;
 void print_welcome_message();
-const char* get_name();
+void get_name();
+void begin_adventure();
 
 int main(){
-    
-    
     print_welcome_message();
     get_name();
-
+    begin_adventure();
     return 0;
 }
 
@@ -16,10 +15,17 @@ void print_welcome_message(){
     printf("Hello and welcome to SPACE ADVENTURES!!!!!\n");
 }
 
-const char* get_name(){
+void get_name(){
     char name[MAX_INPUT_SIZE];
     printf("Please Enter Your Name: ");
     fgets(name, MAX_INPUT_SIZE, stdin);
     printf("\nWelcome %s\n", name);
     
+}
+
+void begin_adventure(){
+    char input[MAX_INPUT_SIZE];
+    int planet = rand() % 10;
+
+    printf("Randomly Chose Planet number%d\n", planet);
 }
