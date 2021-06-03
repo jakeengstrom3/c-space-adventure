084b4cf0bd2d7671b5029b8ff0a06ce03313d445
jakeengstrom3 <jakeengstrom3@DESKTOP-27NSSQD.localdomain>
2021-06-03 15:49:47 -0700

    First attempt at getting input for choosing the planet. It works, but definetly could be refactored. It is integrated with go_to_planet(), which is where I put all the json parsing stuff.

--- main.c
+++ main.c
@@ -9,27 +9,12 @@ void get_name();
 void begin_adventure();
 void get_user_planet();
 void get_random_planet();
+void go_to_planet();
 
 int main(){
-    //print_welcome_message();
-    //get_name();
-    //begin_adventure();
-
-    FILE *fp;
-    char buffer[8192];
-
-    struct json_object *parsed_json;
-    struct json_object *planets;
-
-    fp = fopen("planetarySystem.json", "r");
-    fread(buffer, 8192, 1, fp);
-    fclose(fp);
-
-    parsed_json = json_tokener_parse(buffer);
-    json_object_object_get_ex(parsed_json, "planets", &planets);
-    printf("%ld", json_object_array_length(planets));
-
-
+    print_welcome_message();
+    get_name();
+    begin_adventure();
     return 0;
 }
 
@@ -41,14 +26,17 @@ void get_name(){
     char name[MAX_INPUT_SIZE];
     printf("Please Enter Your Name: ");
     fgets(name, MAX_INPUT_SIZE, stdin);
+    
     printf("\nWelcome %s\n", name);
     
 }
 
 void begin_adventure(){
-    char input[2];
+    char input[4];
     printf("Should I randomly choose a planet to go to? (Y or N)");
-    fgets(input, sizeof(input), stdin);
+    
+    scanf("%s", input);
+    
     char no[] = "N";
     char yes[] = "Y";
     
@@ -77,14 +65,80 @@ void begin_adventure(){
 void get_user_planet(){
     printf("Please Enter the name of the planet you want to go to\n");
 
-    char input[MAX_INPUT_SIZE];
-    fgets(input, sizeof(input), stdin);
+    char input[7];
     
-    
-    
+    scanf("%s", input);
+
+    if(strcmp(input, "Mercury") == 0){
+        printf("Going to planet 0\n");
+        go_to_planet(0);
+    }
+    else if(strcmp(input, "Venus") == 0){
+        printf("Going to planet 1\n");
+        go_to_planet(1);
+    }
+    else if(strcmp(input, "Earth") == 0){
+        printf("Going to planet 2\n");
+        go_to_planet(2);
+    }
+    else if(strcmp(input, "Mars") == 0){
+        printf("Going to planet 3\n");
+        go_to_planet(3);
+    }
+    else if(strcmp(input, "Jupiter") == 0){
+        printf("Going to planet 4\n");
+        go_to_planet(4);
+    }
+    else if(strcmp(input, "Saturn") == 0){
+        printf("Going to planet 5\n");
+        go_to_planet(5);
+    }
+    else if(strcmp(input, "Uranus") == 0){
+        printf("Going to planet 6\n");
+        go_to_planet(6);
+    }
+    else if(strcmp(input, "Neptune") == 0){
+        printf("Going to planet 7\n");
+        go_to_planet(7);
+    }
+    else if(strcmp(input, "Pluto") == 0){
+        printf("Going to planet 8\n");
+        go_to_planet(8);
+    }
+    else{
+        printf("Please enter a valid planet name!\n");
+        get_user_planet();
+    }
+
 }
 
 void get_random_planet(){
     
+}
+
+void go_to_planet(int planet_num){
+    FILE *fp;
+    char buffer[8192];
+
+    struct json_object *parsed_json;
+    struct json_object *planets;
+    struct json_object *planet;
+    struct json_object *name;
+    struct json_object *description;
+
+    fp = fopen("planetarySystem.json", "r");
+    fread(buffer, 8192, 1, fp);
+    fclose(fp);
+
+    parsed_json = json_tokener_parse(buffer);
+    json_object_object_get_ex(parsed_json, "planets", &planets);
+    
+    planet = json_object_array_get_idx(planets, planet_num);
+
+    json_object_object_get_ex(planet, "name", &name);
+    json_object_object_get_ex(planet, "description", &description);
+    
+    printf("Planet %d: %s\n", planet_num, json_object_get_string(name));
+    printf("Description: %s\n", json_object_get_string(description));
 }
 
